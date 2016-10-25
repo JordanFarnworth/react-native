d226d9727c3c90205b04b10f7ede808db4f64c38
Jordan Farnworth <jordanfarn23@gmail.com>
2016-10-24 20:27:58 -0600

    ReactNative. Albums. Rebuilding index.ios.js to understand core code

--- albums/index.ios.js
+++ albums/index.ios.js
@@ -1,16 +1,53 @@
-// Import Library to create a Component
-import React from 'react';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
 import {
+  AppRegistry,
+  StyleSheet,
   Text,
-  AppRegistry
+  View
 } from 'react-native';
 
-// Create a Component
-const App = () => {
-  return(
-    <Text>Some Text</Text>
-  );
-};
+export default class albums extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native - Albums!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.ios.js
+        </Text>
+        <Text style={styles.instructions}>
+          Press Cmd+R to reload,{'\n'}
+          Cmd+D or shake for dev menu
+        </Text>
+      </View>
+    );
+  }
+}
 
-// Render it to iOS Device
-AppRegistry.registerComponent('albums', () => App);
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('albums', () => albums);
